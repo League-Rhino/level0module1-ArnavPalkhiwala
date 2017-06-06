d98d37ef61e6cd028815236ea586f976057d582c
La Liga de Programadores Increibles <ewbrowne743@gmail.com>
2017-06-05 19:55:03 -0700

    For loops
    We made different for loops

---
+++ src/NinetyNineBottlesofMilkontheWall.java
@@ -0,0 +1,9 @@
+
+public class NinetyNineBottlesofMilkontheWall {
+	public static void main(String[] args) {
+
+		for (int i = 99; i > -1; i = i - 1) {
+			System.out.println("There are " + i + " bottles of milk on the wall.");
+		}
+	}
+}
